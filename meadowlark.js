d274794900eb884b0d8facacd61853de9f79643a
Thang Do Minh <thang@Thangs-MBP.local>
2021-07-28 13:03:32 +0700

    Create views and layout

---
+++ meadowlark.js
@@ -0,0 +1,42 @@
+// meadowlark.js
+var express = require("express")
+
+var app = express()
+
+// Set up handlebars view engine
+var handlesbars = require("express-handlebars").create({
+    defaultLayout: "main",
+})
+app.engine("handlebars", handlesbars.engine)
+app.set("view engine", "handlebars")
+
+app.set("port", process.env.PORT || 3000)
+
+app.get("/", (req, res) => {
+    res.render('home')
+})
+
+app.get("/about", (req, res) => {
+    res.render('about')
+})
+
+// Custom 404 page
+app.use((req, res) => {
+    res.status(404)
+    res.render('404') 
+})
+
+// Custom 500 page
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500)
+    res.render('500')
+})
+
+app.listen(app.get("port"), () => {
+    console.log(
+        "Express started on http://localhost:" +
+            app.get("port") +
+            "; press Ctrl-C to terminate."
+    )
+})
